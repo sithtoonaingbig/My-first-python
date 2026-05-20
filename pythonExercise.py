0b164c65e5d60d59c8d354c1c3715cb9d56076dc
sithtoonaingbig <sithtoonaingbig@gmail.com>
2026-05-20 15:00:05 -0700

    pythonExercise

---
+++ pythonExercise.py
@@ -0,0 +1,19 @@
+number = [5,4,3,2,1]
+
+number.reverse()
+
+print(number)
+
+
+numbers = [10,50,20,80,30]
+
+largest = max(numbers)
+
+print(largest)
+
+
+numbers = [1,2,2,3,4,4,5]
+
+unique = set(numbers)
+
+print(unique)
